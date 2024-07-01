d56d86c25574c64c0ab8bcc010757bf737de8e2d
etec <diazagustiin30@gmail.com>
2024-07-01 11:28:28 -0300

    arregle el blueprints de artistas y canciones

--- cancion/artista.py
+++ cancion/artista.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, render_template
 from . import db
-bp = Blueprint('artista', __name__, '/artistas')
+bp = Blueprint('artista', __name__, url_prefix='/artistas')
 
 
 @bp.route('/')
-def artistas():
+def artis():
     base_de_datos = db.get_db()
     consulta = """
         SELECT name FROM artists
--- cancion/genero.py
+++ cancion/genero.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 from . import db
-bp = Blueprint('genero', __name__, '/generos')
+bp = Blueprint('genero', __name__, url_prefix='/generos')
 
 @bp.route('/')
 def generos():
